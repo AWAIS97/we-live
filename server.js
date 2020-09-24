b7b8bfabf238ce1e3db2bbd8ef1dc6e266acf3d2
Awais Hassan <55058852+AWAIS97@users.noreply.github.com>
2020-09-24 12:02:02 +0500

    Uploaded

---
+++ server.js
@@ -0,0 +1,36 @@
+/** @format */
+
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+  // res.render("room");
+  res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 5000);
